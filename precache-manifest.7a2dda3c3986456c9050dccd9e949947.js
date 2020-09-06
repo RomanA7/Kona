80808d8c3a8cd31df4b7904a3e659f6c0e3c57d8
RomanA7 <rompj7@gmail.com>
2020-09-07 02:06:27 +0300

    Updates

---
+++ precache-manifest.7a2dda3c3986456c9050dccd9e949947.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d03faa7a29c93fdd186d25c0a2d6f70",
+    "url": "/kona/index.html"
+  },
+  {
+    "revision": "923a7aaecef7950f7282",
+    "url": "/kona/static/css/main.3fd3aa1f.chunk.css"
+  },
+  {
+    "revision": "320a5435776f80eae8dc",
+    "url": "/kona/static/js/2.3aa7b956.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/kona/static/js/2.3aa7b956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923a7aaecef7950f7282",
+    "url": "/kona/static/js/main.97262512.chunk.js"
+  },
+  {
+    "revision": "d0b72dbce1144a01aeaf",
+    "url": "/kona/static/js/runtime-main.fbb58d09.js"
+  },
+  {
+    "revision": "abbbeef08794ade461aec86a9605baa3",
+    "url": "/kona/static/media/about-us1.abbbeef0.jpg"
+  },
+  {
+    "revision": "08830efa3fcecf01445df1bb139f7b90",
+    "url": "/kona/static/media/background.08830efa.jpg"
+  },
+  {
+    "revision": "e0870db2353c624d5f37b362d46558bb",
+    "url": "/kona/static/media/product-1.e0870db2.png"
+  },
+  {
+    "revision": "c895e0270c4e4e545c1bebef8124af05",
+    "url": "/kona/static/media/product-2.c895e027.png"
+  },
+  {
+    "revision": "31971910abaec4813c11ac71f0a02309",
+    "url": "/kona/static/media/product-3.31971910.png"
+  },
+  {
+    "revision": "9b1f67b9ddc1ea8e3cdc07a996a6f92e",
+    "url": "/kona/static/media/second.9b1f67b9.jpg"
+  }
+]);
